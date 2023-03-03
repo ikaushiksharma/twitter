2590f8b0b9ffe863cb8089cf695050f6a9a45ee1
Kaushik Sharma <90143986+ikaushiksharma@users.noreply.github.com>
2023-03-03 14:13:09 +0530

    session and modify sidebar and input components

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -11,42 +11,68 @@ import {
   EllipsisHorizontalCircleIcon,
   EllipsisHorizontalIcon,
 } from "@heroicons/react/24/outline";
+import { useSession, signIn, signOut } from "next-auth/react";
 export default function Sidebar() {
+  const { data: session } = useSession();
   return (
     <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
+      {/* Twitter Logo */}
       <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
         <Image
-          height="50"
           width="50"
-          alt="logo"
+          height="50"
           src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
-        />
+        ></Image>
       </div>
-      <div className="mt-4 mb-2.5 xl:items-start">
-        <SidebarMenuItem text="Home" Icon={HomeIcon} />
-        <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
-        <SidebarMenuItem text="Notifications" Icon={BellIcon} />
-        <SidebarMenuItem text="Messages" Icon={InboxIcon} />
-        <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
-        <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
-        <SidebarMenuItem text="Profile" Icon={UserIcon} />
-        <SidebarMenuItem text="More" Icon={EllipsisHorizontalCircleIcon} />
-      </div>
-      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold hidden xl:inline text-lg shadow-md hover:brightness-95">
-        Tweet
-      </button>
+      {/* Menu */}
 
-      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
-        <img
-          className="rounded-full h-10 w-10 xl:mr-2"
-          src="https://avatars.githubusercontent.com/u/90143986?v=4"
-        />
-        <div className="leading-5 hidden xl:inline">
-          <h4 className="font-bold">Kaushik Sharma</h4>
-          <p className="text-gray-500">@ikaushiksharma</p>
-        </div>
-        <EllipsisHorizontalIcon className="h-5 w-5 p-0 xl:ml-8 hidden xl:inline" />
+      <div className="mt-4 mb-2.5 xl:items-start">
+        <SidebarMenuItem text="Home" Icon={HomeIcon} active />
+        <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
+        {session && (
+          <>
+            <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+            <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+            <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
+            <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+            <SidebarMenuItem text="Profile" Icon={UserIcon} />
+            <SidebarMenuItem text="More" Icon={EllipsisHorizontalCircleIcon} />
+          </>
+        )}
       </div>
+
+      {/* Button */}
+
+      {session ? (
+        <>
+          <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
+            Tweet
+          </button>
+
+          {/* Mini-Profile */}
+
+          <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+            <img
+              onClick={signOut}
+              src={session.user.image}
+              alt="user-img"
+              className="h-10 w-10 rounded-full xl:mr-2"
+            />
+            <div className="leading-5 hidden xl:inline">
+              <h4 className="font-bold">{session.user.name}</h4>
+              <p className="text-gray-500">@{session.user.username}</p>
+            </div>
+            <EllipsisHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
+          </div>
+        </>
+      ) : (
+        <button
+          onClick={signIn}
+          className="bg-blue-400 text-white rounded-full w-36 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline"
+        >
+          Sign in
+        </button>
+      )}
     </div>
   );
 }
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
@@ -10,10 +10,16 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-  pages: {
-    signin: "/auth/signin",
-  }
-};
 
-export default NextAuth(authOptions);
- 
+  pages: {
+    signIn: "/auth/signin",
+  },
+
+  callbacks: {
+    async session({ session, token }) {
+      session.user.username = session.user.name.split(" ").join("").toLocaleLowerCase();
+      session.user.uid = token.sub;
+      return session;
+    },
+  },
+});
