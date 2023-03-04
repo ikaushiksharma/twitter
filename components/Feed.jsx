f12e1e42295a0acbaaa2fc53b85227b80f3ad550
Kaushik Sharma <90143986+ikaushiksharma@users.noreply.github.com>
2023-03-04 15:29:48 +0530

    mobile view signin option

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -5,9 +5,14 @@ import { AnimatePresence, motion } from "framer-motion";
 import { db } from "../firebase";
 import Input from "./Input";
 import Post from "./Post";
+import { useRecoilState } from "recoil";
+import { userState } from "@/atom/userAtom";
+import { useRouter } from "next/router";
 
 export default function Feed() {
   const [posts, setPosts] = useState([]);
+  const [currentUser] = useRecoilState(userState);
+  const router = useRouter();
   useEffect(
     () =>
       onSnapshot(query(collection(db, "posts"), orderBy("timestamp", "desc")), (snapshot) => {
@@ -19,7 +24,13 @@ export default function Feed() {
     <div className="xl:ml-[370px] border-x border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex py-2 bg-white px-3 sticky top-0 z-50 border-b border-gray-200">
         <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Home</h2>
-        <div className="hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9">
+        <div
+          onClick={() => {
+            if (!currentUser) router.push("/auth/signin");
+            return;
+          }}
+          className="hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9"
+        >
           <SparklesIcon className="h-5" />
         </div>
       </div>
